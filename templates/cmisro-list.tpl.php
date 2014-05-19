15b872ad1e6e2100dcc45c506735391db98c3d8e
Cliff Ingham <inghamn@bloomington.in.gov>
2014-05-19 15:22:56 -0400

    Set up the module framework

---
+++ templates/cmisro-list.tpl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @copyright 2014 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ * @param mixed $variables['reference']
+ */
+?>
+<div class="cmisro">
+<?php
+	print_r($variables['reference']);
+?>
+</div>
