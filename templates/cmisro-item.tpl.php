dc79356da219912a3be3a1fb2ee9b7407c6da6df
Cliff Ingham <inghamn@bloomington.in.gov>
2014-05-22 15:54:01 -0400

    end-of-day commit.  Able to ask for and display CMIS references.  CMIS communication is shaping up.

--- templates/cmisro-item.tpl.php
+++ templates/cmisro-item.tpl.php
@@ -3,12 +3,12 @@
  * @copyright 2014 City of Bloomington, Indiana
  * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
- * @param mixed $variables['folder']
+ * @param mixed $variables['CMISObject']
  */
 ?>
 <div class="cmisro">
-	<h2>CMISRO List</h2>
+	<h2>CMISRO Item</h2>
 	<?php
-		print_r($variables['folder']);
+		print_r($variables);
 	?>
 </div>
