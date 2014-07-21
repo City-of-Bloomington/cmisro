6cf25ce1fcee76f356b692cbe8983181d771693e
Cliff Ingham <inghamn@bloomington.in.gov>
2014-07-21 12:04:52 -0400

    Created a directory listing for the browser popup

---
+++ templates/cmisro-browser.tpl.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @copyright 2014 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ * @param mixed $variables['listing']
+ */
+?>
+<div class="cmisro">
+	<h2>CMISRO Browser</h2>
+	<ul>
+	<?php
+		global $base_url;
+		$url = "$base_url/cmisro/browser";
+
+        $ignore = ['.DS_Store'];
+
+        if (isset   ($variables['listing']->objects)) {
+            echo "Found ".count($variables['listing']->objects)." objects\n";
+            foreach ($variables['listing']->objects as $item) {
+                $o = _cmisro_object($item->object);
+
+                if (in_array($o['title'], $ignore)) { continue; }
+
+                $class = _cmisro_class_for_type($o['type']);
+                $title = check_plain($o['title']);
+                echo "<li><a href=\"$url/$o[id]\"><i class=\"$class\"></i>$title</a></li>";
+            }
+        }
+	?>
+	</ul>
+</div>
--- templates/cmisro-list.tpl.php
+++ templates/cmisro-list.tpl.php
@@ -1,5 +1,9 @@
 <?php
 /**
+ * Lists the documents returned from a CMIS query
+ *
+ * This will only list the document objects.  Folders will be ignored.
+ *
  * @copyright 2014 City of Bloomington, Indiana
  * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
  * @author Cliff Ingham <inghamn@bloomington.in.gov>
@@ -13,11 +17,21 @@
 		global $base_url;
 		$download = "$base_url/cmisro/download";
 
-		foreach ($variables['folder'] as $i=>$item) {
-			$class = _cmisro_class_for_type($item['type']);
-			$title = check_plain($item['title']);
-			echo "<li><a href=\"$download/$item[id]\"><i class=\"$class\"></i>$title</a></li>";
-		}
+        $ignore = ['.DS_Store'];
+
+        if (isset   ($variables['folder']->objects)) {
+            echo "Found ".count($variables['folder']->objects)." objects\n";
+            foreach ($variables['folder']->objects as $item) {
+                $o = _cmisro_object($item->object);
+
+                if ($o['type'] == 'cmis:folder')    { continue; }
+                if (in_array($o['title'], $ignore)) { continue; }
+
+                $class = _cmisro_class_for_type($o['type']);
+                $title = check_plain($o['title']);
+                echo "<li><a href=\"$download/$o[id]\"><i class=\"$class\"></i>$title</a></li>";
+            }
+        }
 	?>
 	</ul>
 </div>
